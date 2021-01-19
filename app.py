e1e8445c064ed8777af264ca9db1a1f2316abfa7
nroldanf <nicolas.roldan@mail.escuelaing.edu.co>
2021-01-19 00:39:56 -0500

    add functions to compare two local audio files

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 from resemblyzer import preprocess_wav, VoiceEncoder
 from flask import Flask
+from pathlib import Path
+import numpy as np
 
 app = Flask(__name__)
 
-# server = app.server
+wav_fpath1 = Path("utils/", "voice1.mp3")
+wav_fpath2 = Path("utils/", "voice2.mp3")
+
+wav1 = preprocess_wav(wav_fpath1)
+wav2 = preprocess_wav(wav_fpath2)
+
+encoder = VoiceEncoder("cpu")
+
+speaker_wavs = [wav1, wav2]
 
 @app.route("/verify", methods=["POST"])
 def main():
-    #
-    return {"Exito": "Ha sido un exito"}
+    speaker_embeds = [encoder.embed_utterance(speaker_wav) for speaker_wav in speaker_wavs]
+    result = speaker_embeds[0] @ speaker_embeds[1]
+    return {"Exito": np.float64(result)}
 
 
 if __name__ == '__main__':
